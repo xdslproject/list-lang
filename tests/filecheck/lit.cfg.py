d120312872e75343c12d432446f27248e1073f00
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2025-09-07 17:13:27 +0200

    forgot one parent

--- tests/filecheck/lit.cfg.py
+++ tests/filecheck/lit.cfg.py
@@ -6,7 +6,7 @@ config.name = "list-lang-parser-tests"
 config.test_format = lit.formats.ShTest(execute_external=True)
 config.suffixes = [".lsl"]
 
-project_folder = Path(__file__).parent.parent
+project_folder = Path(__file__).parent.parent.parent
 parse_python_script = project_folder.joinpath("src").joinpath("main.py")
 
 config.substitutions.append((r"%lsl-parser", parse_python_script))
